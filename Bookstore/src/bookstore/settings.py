8b80b0abd06a08ba1a35d921fa21926bef6ea61d
timone019 <143154960+timone019@users.noreply.github.com>
2024-08-30 13:22:58 -0700

    completed 2.6 practice: login, logout and protected features

--- Bookstore/src/bookstore/settings.py
+++ Bookstore/src/bookstore/settings.py
@@ -130,3 +130,6 @@ MEDIA_ROOT= BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH
+LOGIN_URL='/login/'
